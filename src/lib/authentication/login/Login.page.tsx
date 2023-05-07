f3bcbe4d40b67970a4111ed87f6c336fd5135573
Mateusz Cholewa <mateusz.cholewa@emiloo.pl>
2023-05-07 23:13:41 +0200

    Dodany login page
    Dodany rozgrzebany register page

--- src/lib/authentication/login/Login.page.tsx
+++ src/lib/authentication/login/Login.page.tsx
@@ -1,7 +1,99 @@
+import {
+  Alert,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+  Link,
+} from '@mui/material';
+import { Box, Container, Stack } from '@mui/system';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import axios, { AxiosError } from 'axios';
+
 export const LoginPage = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [dispError, setDispError] = React.useState(false);
+  const [errorMsg, setErrorMsg] = React.useState('');
+  const [mail, setMail] = React.useState('');
+  const [pass, setPass] = React.useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    setLoading(true);
+
+    axios
+      .post('http://localhost:2137/login', {
+        email: mail,
+        password: pass,
+      })
+      .then(() => {
+        navigate('/home');
+      })
+      .catch((err: AxiosError) => {
+        setErrorMsg(`Błąd logowania: ${err.message}`);
+        setDispError(true);
+        setLoading(false);
+      });
+  };
+
   return (
     <>
-      <div>Login</div>
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100vw',
+          height: '100vh',
+          alignItems: 'center',
+          background: '#DDDDDD',
+        }}
+      >
+        <Container>
+          <Card>
+            <CardContent>
+              <Stack spacing={2}>
+                <Typography variant="h4">Hello, please login</Typography>
+                <TextField
+                  id="email"
+                  label="Email address"
+                  variant="outlined"
+                  value={mail}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setMail(event.target.value);
+                  }}
+                />
+                <TextField
+                  id="password"
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  value={pass}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPass(event.target.value);
+                  }}
+                />
+                <LoadingButton
+                  size="small"
+                  onClick={handleLogin}
+                  loading={loading}
+                  variant="contained"
+                >
+                  Login
+                </LoadingButton>
+                <Typography margin="normal">
+                  No account?
+                  <Link href="register" underline="always">
+                    Register
+                  </Link>
+                </Typography>
+                {dispError ? <Alert severity="error">{errorMsg}</Alert> : <></>}
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
+      </Box>
     </>
   );
 };
--- src/lib/authentication/register/Register.page.tsx
+++ src/lib/authentication/register/Register.page.tsx
@@ -1,7 +1,144 @@
+import {
+  Alert,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+  Link,
+} from '@mui/material';
+import { Box, Container, Stack } from '@mui/system';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import axios, { AxiosError } from 'axios';
+
 export const RegisterPage = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [dispError, setDispError] = React.useState(false);
+  const [errorMsg, setErrorMsg] = React.useState('');
+  const [pass, setPass] = React.useState('');
+  const [passRepeat, setPassRepeat] = React.useState('');
+  const [authors, setAuthors] = React.useState([]);
+
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    setLoading(true);
+
+    axios
+      .post('http://localhost:2137/register', {
+        email,
+        password,
+        name,
+        lastname,
+        phone,
+        address_id,
+      })
+      .then(() => {
+        navigate('/login');
+      })
+      .catch((err: AxiosError) => {
+        setErrorMsg(`Błąd rejestracji: ${err.message}`);
+        setDispError(true);
+        setLoading(false);
+      });
+  };
+
+  axios
+    .get('http://localhost:2137/author')
+    .then((resAuthors) => {
+      setAuthors(resAuthors);
+    })
+    .catch((err) => {});
+
   return (
     <>
-      <div>Register</div>
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100vw',
+          height: '100vh',
+          alignItems: 'center',
+          background: '#DDDDDD',
+        }}
+      >
+        <Container>
+          <Card>
+            <CardContent>
+              <Stack spacing={2}>
+                <Typography variant="h4">New account</Typography>
+                <TextField
+                  id="email"
+                  label="Email address"
+                  variant="outlined"
+                  value={mail}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setMail(event.target.value);
+                  }}
+                />
+                <TextField
+                  id="password"
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  value={pass}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPass(event.target.value);
+                  }}
+                />
+                <TextField
+                  id="password-repeat"
+                  label="Repeat password"
+                  type="password"
+                  variant="outlined"
+                  value={pass}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPass(event.target.value);
+                  }}
+                />
+                <TextField
+                  id="lastname"
+                  label="lastname"
+                  variant="outlined"
+                  value={pass}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPass(event.target.value);
+                  }}
+                />
+                <TextField
+                  id="phone"
+                  label="phone"
+                  variant="outlined"
+                  value={pass}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPass(event.target.value);
+                  }}
+                />
+
+                <TextField
+                  id="name"
+                  label="name"
+                  variant="outlined"
+                  value={pass}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPass(event.target.value);
+                  }}
+                />
+
+                <LoadingButton
+                  size="small"
+                  onClick={handleRegister}
+                  loading={loading}
+                  variant="contained"
+                >
+                  Register
+                </LoadingButton>
+                {dispError ? <Alert severity="error">{errorMsg}</Alert> : <></>}
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
+      </Box>
     </>
   );
 };
